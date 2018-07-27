ea058a2885ec73a64087b77b3458260c94131cce
Edward Wang <marksman@unicornmarkets.com>
2018-07-27 17:21:12 -0500

    modify algorithm of two sum

--- Edward/1_two_sum.py
+++ Edward/1_two_sum.py
@@ -1,14 +1,19 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        if nums == None or target == None:
-            return None
-        length = len(nums)
-        for i in range(length):
-            for j in range(i + 1, length, 1):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+def reverse(x):
+    """
+    :type x: int
+    :rtype: int
+    """
+    sign = (1, -1)[x < 0]
+    print sign
+    rst = sign * int(str(abs(x))[::-1])
+    return rst if -(2 ** 31) - 1 < rst < 2 ** 31 else 0
+
+a = reverse(123)
+print a
+
+a = [1, -1][90 < 0]
+print a
+
+b = cmp(90, 0)
+c = cmp(-85, 0)
+print b, c
