355f4e566031142619cd8ebcc0ce281c2aa12811
Nathan <zhanghengzhezy@gmail.com>
2018-07-29 00:06:33 -0500

    Q28 solution

---
+++ Nathan/28_implement_strStr.py
@@ -0,0 +1,18 @@
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if not needle:
+            return 0
+        elif len(needle) > len(haystack):
+            return -1
+        
+        for i in range(len(haystack)-len(needle)+1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+            
+        return -1
+    
