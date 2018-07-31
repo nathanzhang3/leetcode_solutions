3172512a6e0579ed6a861275fcd1ab54b9818074
Nathan <zhanghengzhezy@gmail.com>
2018-07-31 17:15:19 -0500

    Q101 solution

---
+++ Nathan/101_symmetric_tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root is None:
+            return True
+        else:
+            return self.isMirror(root.left, root.right)
+        
+    def isMirror(self, left, right):
+        if left is None and right is None:
+            return True
+        elif left is None or right is None:
+            return False
+        elif left.val != right.val:
+            return False
+        else:
+            outPair = self.isMirror(right.right, left.left)
+            inPair = self.isMirror(right.left, left.right)
+            return outPair and inPair
